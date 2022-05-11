3bd6e3c3ce414d20837a41cd6817ae95eb3b794a
Diego_M <diegofernandomzlz@gmail.com>
2022-05-11 01:34:06 +0000

    2

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,5 +1,7 @@
 import React, { useContext, useState} from "react";
 import { Context } from "../store/appContext";
+import registroimg from "../../img/registroimg.jpg";
+import { Link, Redirect } from "react-router-dom";
 
 
 export const Register = () => {
@@ -27,157 +29,204 @@ export const Register = () => {
   const testemail = regexemail.test(email)
   const testpassword = regexpassword.test(password)
 
+  
+
   const handleSubmit = (e) => {actions.postRegister(registerUser),e.preventDefault();}
   const preventDef = (e) => {e.preventDefault();}
+  
+    
+    
   //
   let colorName = ""
   if (testname == true) {
-    colorName = "border border-success"
+    colorName = "form-control form-control-lg border border-success"
   }
     else
-    colorName = "border border-danger"
+    colorName = "form-control form-control-lg border border-danger"
   //  
   let colorEmail = ""
   if (testemail == true) {
-    colorEmail = "border border-success"
+    colorEmail = "form-control form-control-lg border border-success"
   }
   else
-    colorEmail = "border border-danger"
+    colorEmail = "form-control form-control-lg border border-danger"
   //  
   let colorPassword = ""
   if (testpassword == true) {
-    colorPassword = "border border-success"
+    colorPassword = "form-control form-control-lg border border-success"
   }
   else
-    colorPassword = "border border-danger"  
+    colorPassword = "form-control form-control-lg border border-danger"  
 
   const onSubmit = (a,b) => {
     if (testname == true && testemail == true && testpassword == true){
       handleSubmit(a,b)
-
+        
     }
     else
       preventDef(a)
 
 
+
   }
   return (
-    <div className="text-center">
-      <h2 className="mt-4">Registrate</h2>
-      <hr />
-      <br />
-      <p>
-        Únete a nuestra red de músicos TOTALMENTE GRATIS y ayúdanos a promover a
-        los músicos y las bandas en todos los lugares.
-      </p>
+    <section className="h-100 bg-light">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col">
+            <div className="card card-registration my-4">
+              <div className="row g-0">
+                <div className="col-xl-6 d-none d-xl-block">
+                  <img src={registroimg} className="imgRegister" />
+                </div>
+                <div className="col-xl-6">
+                  <div className="card-body p-md-5 text-black">
+                    <h3 className="mb-5 text-uppercase">Registrate</h3>
 
-      <form className="text-start ms-5">
-        <div className="mb-3 py-5">
-          <h4>Como quieres que te llamemos?</h4>
-          <br />
-          <input onChange={handleName} value={name}
-            id="name"
-            for="exampleInputEmail1"
-            className={colorName}
-            placeholder="Ingrese un usuario"
-          />
-          <h4 className="mt-3 ">Cual es tu correo electronico?</h4>
-          <br />
-          <input onChange={handleEmail} value={email}
-            id="email"
-            for="exampleInputEmail1"
-            className={colorEmail}
-            placeholder="Ingresa tu correo"
-          />
-          <div id="emailHelp" className="form-text">
-            Nunca compartiremos tu correo con un tercero.
+                    <div className="row">
+                      <div className="form-outline mb-4">
+                        <label className="form-label">
+                          Como quieres que te llamemos?
+                        </label>
+                        <div className="form-outline mb-4">
+                          <input
+                            onChange={handleName}
+                            value={name}
+                            type="text"
+                            className={colorName}
+                            placeholder="Ingrese un usuario"
+                          />
+                        </div>
+                      </div>
+                      <div className="form-outline mb-4">
+                        <div className="form-outline">
+                          <label className="form-label">
+                            Ingresa tu correo electronico
+                          </label>
+                          <input
+                            onChange={handleEmail}
+                            value={email}
+                            type="text"
+                            className={colorEmail}
+                            placeholder="Email"
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <label className="form-label">Crea una contraseña</label>
+                      <input
+                        onChange={handlePassword}
+                        value={password}
+                        type="text"
+                        className={colorPassword}
+                        placeholder="Password segura:(OlitadeMar123@)"
+                      />
+                      <p class="text-muted">debe contener Mayus minus numeros un simbolo y 8 caracteres</p>
+                    </div>
+                   
+
+                    <div className="d-md-flex justify-content-start align-items-center mb-4 py-2">
+                      <h6 className="mb-0 me-4">Genero: </h6>
+
+                      <div className="form-check form-check-inline mb-0 me-4">
+                        <input
+                          onChange={handlegender}
+                          className="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions"
+                          id="femaleGender"
+                          value="female"
+                        />
+                        <label className="form-check-label">Mujer</label>
+                      </div>
+
+                      <div className="form-check form-check-inline mb-0 me-4">
+                        <input
+                          onChange={handlegender}
+                          className="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions"
+                          id="maleGender"
+                          value="male"
+                        />
+                        <label className="form-check-label">Hombre</label>
+                      </div>
+
+                      <div className="form-check form-check-inline mb-0">
+                        <input
+                          onChange={handlegender}
+                          className="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions"
+                          id="otherGender"
+                          value="non-binary"
+                        />
+                        <label className="form-check-label">No binario</label>
+                      </div>
+                    </div>
+
+                    <div className="col-md-6 mb-4">
+                      <form>
+                        <label>Ingrese fecha de nacimiento:</label>
+                        <input
+                          onChange={handleBirthDate}
+                          value={date_of_birth}
+                          type="date"
+                          name="birthday"
+                        />
+                      </form>
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <div className="col-md-6 mb-4">
+                        <label>Selecciona tu genero favorito:</label>
+
+                        <select
+                          onChange={handlefavMusicgenre}
+                          value={favMusicgenre}
+                        >
+                          <option value="rock">Rock</option>
+                          <option value="pop">Pop</option>
+                          <option value="rap">Rap</option>
+                          <option value="electronica">Electronica</option>
+                          <option value="idependiente">Independiente</option>
+                          <option value="clasica">Clasica</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className="d-flex justify-content-center pt-3">
+                      <button
+                        onClick={onSubmit}
+                        type="submit "
+                        id="buttonRegister liveAlertBtn"
+                        className="btn btn-warning btn-lg ms-2 rounded-pill"
+                      >
+                        Registrarme
+                      </button>
+                    </div>
+                    <div className="mb-3 form-check mt-3 d-flex justify-content-center">
+                      <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="exampleCheck1"
+                      />
+                      <div className="form-check-label ms-3 ">
+                        He leido y acepto las
+                        <Link to="/privacy" className="ms-1">
+                          politicas de privacidad.
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-
-          <h4 className="mt-3">Crea una contrasena</h4>
-          <br />
-          <input
-          id="password"
-           onChange={handlePassword} value={password}
-            for="exampleInputEmail1"
-            className={colorPassword}
-            placeholder="Contrasena"
-          />
-
-          <div id="emailHelp" className="form-text">
-            Tu contraseña debe ser alfanumerica, contener una Mayuscula, una minuscula, un (!@#$%^&*) y ser de almenos 8 caracteres.
-          </div>
-
-          <h4 className="mt-3">Cual es tu genero?</h4>
-          <div className="form-check form-check-inline">
-            <input onChange={handlegender}
-              className="form-check-input"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio1"
-              value="Male"
-            />
-            <label className="form-check-label" for="inlineRadio1">
-              Hombre
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input onChange={handlegender}
-              className="form-check-input"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio2"
-              value="Female"
-            />
-            <label className="form-check-label" for="inlineRadio2">
-              Mujer
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input onChange={handlegender}
-              className="form-check-input"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio2"
-              value="Other"
-            />
-            <label className="form-check-label" for="inlineRadio2">
-              No Binario
-            </label>
-          </div>
-
-          <h4 className="mt-3">Cual es tu fecha de nacimiento</h4>
-          <form>
-            <label>Seleccione la fecha:</label>
-            <input type="date" name="birthday" onChange={handleBirthDate} value={date_of_birth} />
-          </form>
-
-          <div className="mb-3 form-check mt-3">
-          <div className="form-outline mb-4">
-          <div className="col-md-6 mb-4">
-          <label>Selecciona tu genero favorito:</label>
-
-          <select onChange={handlefavMusicgenre} value={favMusicgenre}>
-          <option value="rock">Rock</option>
-          <option value="pop">Pop</option>
-          <option value="rap">Rap</option>
-          <option value="electronica">Electronica</option>
-          <option value="idependiente">Independiente</option>
-          <option value="clasica">Clasica</option>
-          </select>
-          </div>
-          </div>
-
-
-            
-            <label className="form-check-label " for="exampleCheck1">
-              He leido y acepto las politicas de privacidad
-            </label>
-          </div>
-          <button type="submit" className="btn btn-primary" onClick={onSubmit}>
-            Registrarme
-          </button>
         </div>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 };
